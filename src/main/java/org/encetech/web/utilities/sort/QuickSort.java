70c1433b2212a63d7c2ae2c7c2c1eab9379993e9
Christian J. Gonzalez <c.gonzalez@encetech.org>
2024-01-26 19:52:17 -0700

    Update .gitignore and add QuickSort implementation
    .idea directory has been added to .gitignore to avoid tracking unnecessary project files. Meanwhile, QuickSort has been implemented in the org.encetech.web.utilities.sort package, providing a more efficient sorting method for data structures. The new implementation also includes a success message for completed sorting tasks.

--- src/main/java/org/encetech/web/utilities/sort/QuickSort.java
+++ src/main/java/org/encetech/web/utilities/sort/QuickSort.java
@@ -1,3 +1,53 @@
 package org.encetech.web.utilities.sort;
+/*
+*Web Utilities
+* Quick Sort
+* Author: Christian J Gonzalez
+* Time Complexity: O(n*log(n) )
+ */
+public class QuickSort<T extends Comparable<T>> {
+    private final String SUCCESS_MESSAGE = "Quick Sort Successful";
 
-public class QuickSort {}
+    private T[] array;
+    private int arraySize;
+
+    private String completed() {
+        return SUCCESS_MESSAGE;
+    }
+    public String sort(T[] array) {
+        this.array = array;
+        this.arraySize = array.length;
+        partition(0, arraySize - 1, arraySize - 1);
+        return this.completed();
+    }
+    private void partition(int low, int high, int pivot) {
+        int i = low;
+        int j = high;
+        while (i <= j) {
+            while (array[i].compareTo(array[pivot]) < 0) {
+                i++;
+            }
+            while (array[j].compareTo(array[pivot]) > 0) {
+                j--;
+            }
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        if (low < j) {
+            partition(low, j, j);
+        }
+        if (i < high) {
+            partition(i, high, i);
+        }
+    }
+    private void swap(int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+}
